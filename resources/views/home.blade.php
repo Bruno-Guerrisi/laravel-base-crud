fcf8c058bacc3be00c24acdb92cd9cdb795a95e0
Bruno-Guerrisi <brunoguerrisi1973@outlook.it>
2022-01-26 16:05:27 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('main-content')
+
+<section class="mb-4">
+    <h1>contente here</h1>
+</section>
+
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center bg-primary text-white p-3">
+    <h6>Made with boolean</h6>
+</footer>
